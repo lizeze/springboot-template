27b8f0b0ea93b4503ec3f5f919815afadc2a80d0
zeze.li <lizeze.boy@gmail.com>
2021-09-20 14:13:17 +0800

    first commit

---
+++ src/main/java/cn/xx996/springboottemplate/SpringbootTemplateApplication.java
@@ -0,0 +1,13 @@
+package cn.xx996.springboottemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/xx996/springboottemplate/SpringbootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xx996.springboottemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
